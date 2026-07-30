cda69ab33b0811941420e0f5d2f051bd3e438a01
rcray152828-creator <rcray152828@gmail.com>
2026-07-30 20:08:19 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-// BhaktiCounter Script
+let count = 0;
+let total = 0;
+let today = 0;
+
+const countEl = document.getElementById("count");
+const malaEl = document.getElementById("mala");
+const totalEl = document.getElementById("total");
+const todayEl = document.getElementById("today");
+const btn = document.getElementById("jaapBtn");
+
+btn.addEventListener("click", function () {
+    count++;
+    total++;
+    today++;
+
+    countEl.innerText = count;
+    totalEl.innerText = total;
+    todayEl.innerText = today;
+    malaEl.innerText = Math.floor(total / 108);
+});
